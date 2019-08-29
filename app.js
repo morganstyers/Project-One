03d9a9ed54077f51a53fd0dc11b3adbec2da7095
Pat Perry <risingphx1@gmail.com>
2019-08-28 20:38:52 -0400

    added jquery script access

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 console.log("linked")
 
-$("#submit-button").on("click", function () {
+$(".btn").on("click", function () {
 
     var city = "charlotte"//$("#city-input");
     var state = "north carolina"//$("#state-input");
-    var queryurl = "https:api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
+    var queryurl = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
 console.log(queryurl)
 
 });
