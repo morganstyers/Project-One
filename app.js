4e28d4c39ecd019e16569ff46f64f41baed7f304
Madison Ofarril <mofarril.mof@gmail.com>
2019-08-28 20:34:12 -0400

    carlo made changes to pg2.html

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ $("#submit-button").on("click", function () {
 
     var city = "charlotte"//$("#city-input");
     var state = "north carolina"//$("#state-input");
-    var queryurl = "https:api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
+    var queryurl = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
 console.log(queryurl)
 
 });
 
-console.log("linked")
