3ad9defaf3bbaf278a6d58c4283a889c9dadeff4
Carlo Ortega <cfor0@outlook.com>
2019-09-02 18:01:25 -0400

    added more

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 console.log("linked")
 
-$("submit-button").on("click", function () {
+$("#start").on("click", function () {
     event.preventDefault();
     var city = "charlotte"//$("#city-input");
     var state = "north_carolina"//$("#state-input");
     var queryurl = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
 console.log(queryurl)
-.ajax({
+$.ajax({
     url: queryurl,
     method: "GET"
 })
 
 .then(function(response){
     var results = response;
+    var answer = [];
+
     for (var i = 0; i < results.length; i++){
-        // retrieve first lat/lon and return into variable
-       if (results.longitude === null || results.longitude === "") {
+        // retrieve first lat/lon and push into array
+       if (results[i].longitude === null || results[i].longitude === "") {
            i++
        } else {
-           return(i)
+            answer.push(results[i].longitude)
        }
+       console.log(results[i].longitude)
+       console.log(answer)
         // 
     }
 })
