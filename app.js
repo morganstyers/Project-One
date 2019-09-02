2449dabc0823b58cad9b876272854414b20a2d55
Carlo Ortega <cfor0@outlook.com>
2019-09-02 17:17:37 -0400

    created for loop function for brewery location

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 console.log("linked")
 
-$("#submit-button").on("click", function () {
-
+$("submit-button").on("click", function () {
+    event.preventDefault();
     var city = "charlotte"//$("#city-input");
-    var state = "north carolina"//$("#state-input");
-    var queryurl = "https:api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
+    var state = "north_carolina"//$("#state-input");
+    var queryurl = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state;
 console.log(queryurl)
+.ajax({
+    url: queryurl,
+    method: "GET"
+})
 
+.then(function(response){
+    var results = response;
+    for (var i = 0; i < results.length; i++){
+        // retrieve first lat/lon and return into variable
+       if (results.longitude === null || results.longitude === "") {
+           i++
+       } else {
+           return(i)
+       }
+        // 
+    }
+})
 });
 
-console.log("linked")
+
