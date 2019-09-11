5fb888ba036f1af70e5d644e1890ff6f5ded6eb7
Morgan Styers <morganstyers@gmail.com>
2019-09-11 02:00:26 -0400

    modify

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log("linked")
 
 $(document).ready(function () {
   $("#section2").hide();
@@ -10,9 +9,7 @@ $(document).ready(function () {
 
   $("#back").on('click', function () {
     $("#section2").hide()
-    $("#section1").show()
   })
-  
   function beerRise() {
     $('.beer').addClass('fill');
     $('.head').addClass('active');
@@ -30,165 +27,70 @@ $(document).ready(function () {
     pourBeer();
   }, 2000)
 
+
+
   $("#getLocation").on("click", function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
+      navigator.geolocation.getCurrentPosition(showPosition)
     }
   });
+
+  var name; 
+  var address;
+  var phone;
+  var website;
+  var id;
+  var city;
+  var state;
 
   function showPosition(position) {
     var latCoord = position.coords.latitude;
     var longCoord = position.coords.longitude;
-    console.log(latCoord);
-    console.log(longCoord);
-
-    var queryurl = "https://api.tomtom.com/search/2/categorySearch/company,brewery.json?key=Ab4s6zW0kUp03AlC2DusRDhwK6rkp5Ap&lat=" + latCoord + "&lon=" + longCoord + "&radius=32186&limit=5"
+    var queryURL = "https://api.tomtom.com/search/2/categorySearch/company,brewery.json?key=Ab4s6zW0kUp03AlC2DusRDhwK6rkp5Ap&lat=" + latCoord + "&lon=" + longCoord + "&radius=13094&limit=5"
     $.ajax({
-      url: queryurl,
+      url: queryURL,
       method: "GET"
-    })
-      .then(function (response) {
-        // put into html
-        for (var i = 0; i < response.results.length; i++) {
-          var name = response.results[i].poi.name;
-          var address = response.results[i].address.freeformAddress;
-          var phone = response.results[i].poi.phone;
-          var website = response.results[i].poi.url;
-          var city = response.results[i].address.municipality;
-          var state = response.results[i].address.countrySubdivision;
+    }).then(function (response) {
+      for (var i = 0; i < response.results.length; i++) {
+        name = response.results[i].poi.name;
+        address = response.results[i].address.freeformAddress;
+        phone = response.results[i].poi.phone;
+        website = response.results[i].poi.url;
+        city = response.results[i].address.municipality;
+        state = response.results[i].address.countrySubdivision;
+      
 
+      console.log(address);
+      console.log(name);
+      console.log(phone);
 
-          console.log(address);
-          console.log(name);
-          console.log(phone);
+      var breweryData = {
+        name: name,
+        address:address,
+        phone: phone,
+        website: website.link(website),
+      }
+     
+      console.log("Address: " + breweryData.address);
+      console.log(breweryData.name);
+      console.log(breweryData.phone);
+      console.log(breweryData);
+      
+      // console.log(city);
+      // console.log(state);
+      var newDiv = $("<div class='response'>");
+      var newUl = $("<ul>");
+      var newLi = $("<li>").html(breweryData.name + "<br>" + breweryData.address + "<br>" + breweryData.phone + "<br>" + breweryData.website);
 
-          var newDiv = $("<div>");
-          var newUl = $("<ul>");
-          var newLi = $("<li>");
+      // newLi.html(name);
+      // newLi.html(address);
+      // newLi.html(phone);
 
-          newLi.html(address, name, phone);
-          newUl.append(newLi);
-          newDiv.append(newUl);
+      newUl.append(newLi);
+      newDiv.append(newUl);
 
-          $(".card-text").append(newDiv)
-
-          console.log("logging brewery " + name);
-
-
-          /*    var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
-             console.log(queryurl)
-             $.ajax({
-                 url: queryurl,
-                 method: "GET",
-                 "headers": {
-                     "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
-                 }
-             })
-                 .then(function (response) {
-                     console.log(response);
-                     console.log(response.businesses.length);
-                 }); */
-
-          console.log(response);
-          console.log(address);
-          console.log(name);
-          console.log(phone);
-          console.log(city);
-          console.log(state);
-
-
-          var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
-          console.log(queryurl)
-          $.ajax({
-            url: queryurl,
-            method: "GET",
-            "headers": {
-              "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
-            }
-          })
-            .then(function (response) {
-              console.log(response);
-              console.log(response.businesses.length);
-              for (var i = 0; i < response.businesses.length; i++) {
-                var id = response.businesses[i].id
-                console.log("yelp id" + id)
-
-                var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id + "/reviews"
-                console.log(queryurl)
-                $.ajax({
-                  url: queryurl,
-                  method: "GET",
-                  "headers": {
-                    "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
-                  }
-                })
-                  .then(function (response) {
-                    console.log(response);
-
-                    var reviewUrl = response.reviews[0].url;
-                    console.log("Yelp URL " + reviewUrl);
-
-
-                  });
-              }
-
-                    for (var i = 0; i < response.reviews.length; i++) {
-                      var reviewUrl = response.reviews[i].url;
-                      console.log("Yelp URL " + reviewUrl);
-
-                      var reviewInfo = {
-                        name: name,
-                        url: reviewUrl,
-                      };
-                      console.log("brewery name " + reviewInfo.name);
-                      console.log(reviewInfo.url);
-
-                   }
-                  });
-             }
-
-            });
-
-          if (phone === null || phone === undefined || website === null || website === undefined) {
-            phone = "";
-            website = "";
-          } else if (!website.includes("https://")) {
-            website = "https://" + website
-          }
-
-          var breweryData = {
-            name: name,
-            address: address,
-            phone: phone,
-            website: website.link(website),
-          };
-          console.log("Address: " + breweryData.address);
-          console.log(breweryData.name);
-          console.log(breweryData.phone);
-          console.log(breweryData);
-          // console.log(city);
-          // console.log(state);
-          var newDiv = $("<div class=response>");
-          var newUl = $("<ul>");
-          var newLi = $("<li>").html(breweryData.name + "<br>" + breweryData.address + "<br>" + breweryData.phone + "<br>" + breweryData.website);
-
-          // newLi.html(name);
-          // newLi.html(address);
-          // newLi.html(phone);
-
-          newUl.append(newLi);
-          newDiv.append(newUl);
-
-          $(".card-body").append(newDiv)
-
-          $("#review").on('click', function () {
-            $(".response").hide();
-          })
-          // var reviewDiv = $("<div class=review");
-          // var reviewUl = $("<ul>");
-          // var reviewLi = $("<li>");
-          // console.log(reviewInfo + "review")
-
-        }
-      });
+      $(".card-body").append(newDiv)
+    }
+  })
+}
+});
