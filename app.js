385302aeeac85c3c786d1a4b990956d9dc56309b
Morgan Styers <morganstyers@gmail.com>
2019-09-10 21:39:55 -0400

    modify

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 
 console.log("linked")
 
-$("#section2").hide();
-$("#start").on("click", function () {
-  $("#section2").show()
- 
-});
-
-
 $(document).ready(function () {
-  //beer animation for home page
- 
+  $("#section2").hide();
 
+  $("#start").on("click", function () {
+    $("#section2").show()
+    $("#section1").hide()
+  });
+
+  $("#back").on('click', function () {
+    $("#section2").hide()
+  })
+  
   function beerRise() {
     $('.beer').addClass('fill');
     $('.head').addClass('active');
@@ -28,9 +29,6 @@ $(document).ready(function () {
   setTimeout(function () {
     pourBeer();
   }, 2000)
-
-  //end beer animation
-  //start js 
 
   $("#getLocation").on("click", function getLocation() {
     if (navigator.geolocation) {
@@ -60,71 +58,71 @@ $(document).ready(function () {
           var city = response.results[i].address.municipality;
           var state = response.results[i].address.countrySubdivision;
 
-        console.log(address);
-        console.log(name);
-        console.log(phone);
+          console.log(address);
+          console.log(name);
+          console.log(phone);
 
-        var newDiv = $("<div>");
-        var newUl = $("<ul>");
-        var newLi = $("<li>");
+          var newDiv = $("<div>");
+          var newUl = $("<ul>");
+          var newLi = $("<li>");
 
-        newLi.html(address, name, phone);
-        newUl.append(newLi);
-        newDiv.append(newUl);
+          newLi.html(address, name, phone);
+          newUl.append(newLi);
+          newDiv.append(newUl);
 
-        $(".card-text").append(newDiv)
+          $(".card-text").append(newDiv)
 
-        console.log("logging brewery " + name);
+          console.log("logging brewery " + name);
 
 
-     /*    var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
-        console.log(queryurl)
-        $.ajax({
+          /*    var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
+             console.log(queryurl)
+             $.ajax({
+                 url: queryurl,
+                 method: "GET",
+                 "headers": {
+                     "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
+                 }
+             })
+                 .then(function (response) {
+                     console.log(response);
+                     console.log(response.businesses.length);
+                 }); */
+
+          var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
+          console.log(queryurl)
+          $.ajax({
             url: queryurl,
             method: "GET",
             "headers": {
-                "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
+              "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
             }
-        })
+          })
             .then(function (response) {
-                console.log(response);
-                console.log(response.businesses.length);
-            }); */
+              console.log(response);
+              console.log(response.businesses.length);
+              for (var i = 0; i < response.businesses.length; i++) {
+                var id = response.businesses[i].id
+                console.log("yelp id" + id)
 
-            var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
-            console.log(queryurl)
-            $.ajax({
-                url: queryurl,
-                method: "GET",
-                "headers": {
+                var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id + "/reviews"
+                console.log(queryurl)
+                $.ajax({
+                  url: queryurl,
+                  method: "GET",
+                  "headers": {
                     "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
-                }
-            })
-                .then(function (response) {
+                  }
+                })
+                  .then(function (response) {
                     console.log(response);
-                    console.log(response.businesses.length);
-                    for (var i =0; i < response.businesses.length; i++) {
-                      var id = response.businesses[i].id
-                      console.log("yelp id" + id)
+                    var reviewUrl = response.reviews[0].url;
+                    console.log("Yelp URL " + reviewUrl);
 
-                      var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id + "/reviews"
-                      console.log(queryurl)
-                      $.ajax({
-                          url: queryurl,
-                          method: "GET",
-                          "headers": {
-                              "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
-                          }
-                      })
-                          .then(function (response) {
-                              console.log(response);
-                              var reviewUrl = response.reviews[0].url;
-                              console.log("Yelp URL " + reviewUrl);
-                              
-        
-                          });
-                    }
-                });
+
+                  });
+              }
+            });
 
           if (phone === null || phone === undefined || website === null || website === undefined) {
             phone = "";
@@ -160,9 +158,4 @@ $(document).ready(function () {
         }
       });
   };
-
-
-    $("getLocation").on('click', function () {
-      
-    })
 });
