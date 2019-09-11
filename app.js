c87493e55ac495bc9d1dafc91db0387772c06bb4
Pat Perry <risingphx1@gmail.com>
2019-09-10 21:00:32 -0400

    working on the review link

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
     console.log(latCoord);
     console.log(longCoord);
 
-    var queryurl = "https://api.tomtom.com/search/2/categorySearch/company,brewery.json?key=Ab4s6zW0kUp03AlC2DusRDhwK6rkp5Ap&lat=" + latCoord + "&lon=" + longCoord + "&radius=13094&limit=5"
+    var queryurl = "https://api.tomtom.com/search/2/categorySearch/company,brewery.json?key=Ab4s6zW0kUp03AlC2DusRDhwK6rkp5Ap&lat=" + latCoord + "&lon=" + longCoord + "&radius=32186&limit=5"
     $.ajax({
       url: queryurl,
       method: "GET"
@@ -56,61 +56,56 @@ $(document).ready(function () {
           var city = response.results[i].address.municipality;
           var state = response.results[i].address.countrySubdivision;
 
-        console.log(address);
-        console.log(name);
-        console.log(phone);
+          console.log(response);
+          console.log(address);
+          console.log(name);
+          console.log(phone);
+          console.log(city);
+          console.log(state);
 
-    
 
-
-     /*    var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
-        console.log(queryurl)
-        $.ajax({
+          var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
+          console.log(queryurl)
+          $.ajax({
             url: queryurl,
             method: "GET",
             "headers": {
-                "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
+              "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
             }
-        })
+          })
             .then(function (response) {
-                console.log(response);
-                console.log(response.businesses.length);
-            }); */
+              console.log(response);
+              console.log(response.businesses.length);
+              for (var i = 0; i < response.businesses.length; i++) {
+                var id = response.businesses[i].id
+                console.log("yelp id" + id)
 
-            var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/matches?name=" + name + "&address1=" + address + "&city=charlotte" + "&state=NC" + "&country=US"
-            console.log(queryurl)
-            $.ajax({
-                url: queryurl,
-                method: "GET",
-                "headers": {
+                var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id + "/reviews"
+                console.log(queryurl)
+                $.ajax({
+                  url: queryurl,
+                  method: "GET",
+                  "headers": {
                     "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
-                }
-            })
-                .then(function (response) {
+                  }
+                })
+                  .then(function (response) {
                     console.log(response);
-                    console.log(response.businesses.length);
-                    for (var i =0; i < response.businesses.length; i++) {
-                      var id = response.businesses[i].id
-                      console.log("yelp id" + id)
+                    for (var i = 0; i < response.reviews.length; i++) {
+                      var reviewUrl = response.reviews[i].url;
+                      console.log("Yelp URL " + reviewUrl);
 
-                      var queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + id + "/reviews"
-                      console.log(queryurl)
-                      $.ajax({
-                          url: queryurl,
-                          method: "GET",
-                          "headers": {
-                              "Authorization": "Bearer 9EO74UpComrdvt4WzJLX9S6xhIZHAqqNiB4T6PaJBGWkMOKiVrGrJDBX7RHZx60BUzK2VrVgcL42klLPExym9hxFz5aYqpjfB0UJj3DLMWpRkhBuVXa4OtJ9dMN2XXYx",
-                          }
-                      })
-                          .then(function (response) {
-                              console.log(response);
-                              var reviewUrl = response.reviews[0].url;
-                              console.log("Yelp URL " + reviewUrl);
-                              
-        
-                          });
-                    }
-                });
+                      var reviewInfo = {
+                        name: name,
+                        url: reviewUrl,
+                      };
+                      console.log("brewery name " + reviewInfo.name);
+                      console.log(reviewInfo.url);
+
+                   }
+                  });
+             }
+            });
 
           if (phone === null || phone === undefined || website === null || website === undefined) {
             phone = "";
@@ -131,7 +126,7 @@ $(document).ready(function () {
           console.log(breweryData);
           // console.log(city);
           // console.log(state);
-          var newDiv = $("<div class='response'>");
+          var newDiv = $("<div class=response>");
           var newUl = $("<ul>");
           var newLi = $("<li>").html(breweryData.name + "<br>" + breweryData.address + "<br>" + breweryData.phone + "<br>" + breweryData.website);
 
@@ -143,11 +138,18 @@ $(document).ready(function () {
           newDiv.append(newUl);
 
           $(".card-body").append(newDiv)
+
+          $("#review").on('click', function () {
+            $(".response").hide();
+          })
+          // var reviewDiv = $("<div class=review");
+          // var reviewUl = $("<ul>");
+          // var reviewLi = $("<li>");
+          // console.log(reviewInfo + "review")
+
         }
       });
   };
 
-    $("getLocation").on('click', function () {
-      
-    })
+
 });
